ff10085cf93594c24f9135a15793d31f7cc071f3
syed kernain <hasansyed700@gmail.com>
2023-06-21 03:54:02 +0500

    Header.jsx Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../userContext";
 import { toast } from "react-toastify";
